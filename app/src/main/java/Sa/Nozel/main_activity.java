96e557317a52635b5a3806d3b8af5ed116129e3f
naief <naiefabdulhady-89@hotmail.com>
2020-02-25 21:04:48 +0300

    Initial commit

---
+++ app/src/main/java/Sa/Nozel/main_activity.java
@@ -0,0 +1,16 @@
+package Sa.Nozel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import Sa.Nozel.R;
+
+public class main_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+    }
+}
